de7f0c468054224f21938e53021f24d7bd520b38
Chris Denneen <cdenneen@gmail.com>
2024-05-14 20:52:29 +0000

    Create cdenneen/taps tap

---
+++ Formula/switcher@0.9.1.rb
@@ -0,0 +1,43 @@
+class SwitcherAT091 < Formula
+  desc "Providing the binary executable for the 'switch' script."
+  homepage "https://github.com/danielfoehrKn/kubeswitch"
+  url "https://github.com/danielfoehrKn/kubeswitch.git",
+    tag: "0.9.1",
+    revision: "9f82b5f75bbc51615d97d006df86f1b9939123fd"
+  license "Apache-2.0"
+  head "https://github.com/danielfoehrKn/kubeswitch.git", branch: "master"
+
+  depends_on "bash" => :build
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+
+  def install
+    rm_rf ".brew_home"
+    ldflags = %W[
+      -w
+      -X github.com/danielfoehrkn/kubeswitch/cmd/switcher.version=#{version}
+      -X github.com/danielfoehrkn/kubeswitch/cmd/switcher.buildDate=#{time.iso8601}
+    ]
+
+    ENV.prepend_path "PATH", Formula["coreutils"].opt_libexec/"gnubin" # needs GNU date
+    system "go", "build", *std_go_args(ldflags:), "./cmd/main.go"
+
+    generate_completions_from_executable(bin/"switcher", "completion", base_name: "switcher")
+  end
+
+  def caveats
+    <<~EOS
+      To setup shell integration, add this to your shell configuration:
+      # bash
+      source <(switcher init bash)
+      # zsh
+      source <(switcher init zsh)
+      # fish
+      switcher init fish | source
+    EOS
+  end
+
+  test do
+    system "#{bin}/switcher", "-h"
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Cdenneen Taps
+
+## How do I install these formulae?
+
+`brew install cdenneen/taps/<formula>`
+
+Or `brew tap cdenneen/taps` and then `brew install <formula>`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "cdenneen/taps"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
